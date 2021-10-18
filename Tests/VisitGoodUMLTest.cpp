54b6c4f3a8e426ee6666a776c19c007f8d2c2135
Joe Getzen <getzenjo@msu.edu>
2021-10-18 05:20:31 -0400

    Tested Implementation of GoodUMLVisitor using hollow Good and Bad UML objects

--- Tests/VisitGoodUMLTest.cpp
+++ Tests/VisitGoodUMLTest.cpp
@@ -12,6 +12,9 @@
 #include <BadClassUML.h>
 #include <GoodUMLVisitor.h>
 
+
+const std::wstring Filename = L"data/uml.xml";
+
 using namespace std;
 
 class TestVisitor : public ItemVisitor
@@ -20,10 +23,76 @@ public:
     virtual void VisitGoodUML(GoodClassUML* goodUML) override { mIsGood=true; }
 
     bool mIsGood=false;
+
+    bool IsGood() {return mIsGood;}
 };
 
 TEST(VisitGoodUMLTest, Constructor)
 {
    GoodUMLVisitor real;
    TestVisitor test;
+}
+
+TEST(VisitGoodUMLTest, VisitAccept)
+{
+    Game game;
+    UMLData data(&game);
+    data.LoadData(Filename);
+
+    std::shared_ptr<UMLName> name;
+    std::vector<std::shared_ptr<UMLAttribute>> attributes;
+    std::vector<std::shared_ptr<UMLOperation>> operations;
+
+    GoodUMLVisitor real;
+    TestVisitor test;
+
+    GoodClassUML GCUML(&game,name,attributes,operations);
+
+    GCUML.Accept(&real);
+    GCUML.Accept(&test);
+}
+TEST(VisitGoodUMLTest, VisitIsGood)
+{
+    Game game;
+    UMLData data(&game);
+    data.LoadData(Filename);
+
+    std::shared_ptr<UMLName> name;
+    std::vector<std::shared_ptr<UMLAttribute>> attributes;
+    std::vector<std::shared_ptr<UMLOperation>> operations;
+
+    GoodUMLVisitor real;
+    TestVisitor test;
+
+    GoodClassUML GCUML(&game,name,attributes,operations);
+
+    ASSERT_TRUE(!real.IsGood());
+    ASSERT_TRUE(!test.IsGood());
+
+    GCUML.Accept(&real);
+    GCUML.Accept(&test);
+
+    ASSERT_TRUE(real.IsGood());
+    ASSERT_TRUE(test.IsGood());
+
+    wstring reason;
+    BadClassUML BCUML(&game,name,attributes,operations,reason);
+
+    GoodUMLVisitor realBad;
+    TestVisitor testBad;
+
+    ASSERT_TRUE(!realBad.IsGood());
+    ASSERT_TRUE(!testBad.IsGood());
+
+    BCUML.Accept(&realBad);
+    BCUML.Accept(&testBad);
+
+    ASSERT_TRUE(!realBad.IsGood());
+    ASSERT_TRUE(!testBad.IsGood());
+
+    GCUML.Accept(&realBad);
+    GCUML.Accept(&testBad);
+
+    ASSERT_TRUE(realBad.IsGood());
+    ASSERT_TRUE(testBad.IsGood());
 }
